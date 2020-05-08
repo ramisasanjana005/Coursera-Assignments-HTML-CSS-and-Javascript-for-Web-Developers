f5148a607ceaaeb986dedf3b2cf17f911b97891d
ramisasanjana005 <59784791+ramisasanjana005@users.noreply.github.com>
2020-05-08 22:01:53 +0600

    Add files via upload

---
+++ Module4_Assignment/SpeakHello.js
@@ -0,0 +1,9 @@
+(function(window) {
+    var hello = new Object();
+    var speakWord = "Hello";
+    hello.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.hello = hello;
+})
+(window);
